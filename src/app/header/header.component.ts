26bbed053f10f824863b6bc244b7d46a55c43825
Salam Abul Hawa <salamabuelhawa@gmail.com>
2020-12-23 16:56:59 +0200

    all components are ready now

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core'; // 2. import a component from the core
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})// 1.put this "@component decorator"// 3. add js object to tjis decorator to configure this component
+/*
+ 3. import them inside the app module
+*/
+export class HeaderComponent{
+
+}
