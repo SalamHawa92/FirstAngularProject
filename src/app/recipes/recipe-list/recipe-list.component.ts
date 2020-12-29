0a6e6cce6ac8e933a11ba4e0aa570a18c109286d
Salam Abul Hawa <salamabuelhawa@gmail.com>
2020-12-29 16:29:43 +0200

    before starting with lecture 60

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/spaghetti-puttanesca_1-1ce4e81.jpg?quality=90&resize=440%2C400'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.loveandlemons.com/wp-content/uploads/2020/03/pantry-recipes-2.jpg')
+    // we actually calling the constructor so we need to pass arguments
+  ]; // we added the model we made as a type , couple of objects // we have to import it
 
   constructor() { }
 
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+// export class Ingredient{
+  // how Ingredient should look like?
+  /*public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+  }
+
+}*/
+  // same work as above but with shortcut
+  export class Ingredient{
+    constructor(public name: string, public amount: number){
+
+    }
+  }
